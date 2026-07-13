f94e410aa1e2edba9ceb228755dad028c39b7713
mbnahmadi <mbn.ahmadi78@gmail.com>
2026-07-13 17:27:15 +0330

    add admin

--- ticketsapp/admin.py
+++ ticketsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import TicketModel, OrderModel
 # Register your models here.
+
+
+admin.site.register(TicketModel)
+admin.site.register(OrderModel)
--- tripsapp/admin.py
+++ tripsapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import TripsModel, TerminalModel, VehicleModel, VehicleSectionModel, SeatModel, TripSeatModel
 # Register your models here.
+
+admin.site.register(TripsModel)
+admin.site.register(TerminalModel)
+admin.site.register(VehicleModel)
+admin.site.register(VehicleSectionModel)
+admin.site.register(SeatModel)
+admin.site.register(TripSeatModel)
