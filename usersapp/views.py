461bc35c921f971227fbbf5bf8f707ea60a3748d
mbnahmadi <mbn.ahmadi78@gmail.com>
2026-07-29 20:37:39 +0330

    set permission class for sign up

--- usersapp/views.py
+++ usersapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from django.contrib.auth import get_user_model
 
 from .serializers import SignUpserializer
 
@@ -9,8 +12,11 @@ from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
 
+User = get_user_model()
 
 class SignUpAPIView(APIView):
+    
+    permission_classes = [AllowAny]
     @swagger_auto_schema(request_body=SignUpserializer)
     def post(self, request):
         serializer = SignUpserializer(data=request.data)
@@ -18,3 +24,8 @@ class SignUpAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class SignUpAPIView(generics.CreateAPIView):
+#     query_set = User.objects.all()
+#     serializer_class = SignUpserializer
+#     permission_class = [AllowAny]
